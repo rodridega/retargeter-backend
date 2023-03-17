38fdbf169706312a9af26715d3c2d8fedb39fc1e
Rodrigo <rodridega@gmail.com>
2023-03-17 17:34:33 -0300

    add cors

--- retargeter/settings.py
+++ retargeter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'retargeterBackend',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,12 +44,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOWED_ORIGINS = ['*']
+
 
 ROOT_URLCONF = 'retargeter.urls'
 
