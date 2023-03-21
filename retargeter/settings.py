85aaef92a72b8b01dba238d6250b44056c7c71b5
rodridega <56453875+rodridega@users.noreply.github.com>
2023-03-20 22:32:19 -0300

    delete CORS_ALLOWED_ORIGINS

--- retargeter/settings.py
+++ retargeter/settings.py
@@ -50,9 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    '*'
-]
 
 
 ROOT_URLCONF = 'retargeter.urls'
